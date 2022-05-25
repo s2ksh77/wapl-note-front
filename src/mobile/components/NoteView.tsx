cca61c24a9f31478a71b70f3db09c491c574e822
Daeun Oh <daeun_oh@tmax.co.kr>
2022-05-25 20:34:24 +0900

    feat(note): 챕터 long press 기능 추가

--- src/mobile/components/NoteView.tsx
+++ src/mobile/components/NoteView.tsx
@@ -49,20 +49,7 @@ const NoteView: React.FC = () => {
     }
   }, [noteViewStore.type]);
 
-  const handleCloseButtonPress = useCallback(() => {
-    noteViewStore.toggleMultiSelectMode();
-    deSelectAll();
-  }, []);
-
-  const handleSelectAllPress = () => {
-    selectAll(chapterList.map(chapter => chapter.id).concat(sharedChapterList.map(chapter => chapter.id)));
-  };
-
-  const handleDeselectAllPress = () => {
-    deSelectAll();
-  };
-
-  const handleCreateChapter = useCallback(async name => {
+  const handleChapterCreate = useCallback(async name => {
     // TODO: response 데이터 동기화
     const res = await chapterStore.createChapter(
       new ChapterModel({
@@ -76,6 +63,42 @@ const NoteView: React.FC = () => {
     setIsNewChapterDialogOpen(false);
   }, []);
 
+  const handleClosePress = useCallback(() => {
+    noteViewStore.toggleMultiSelectMode();
+    deSelectAll();
+  }, []);
+
+  const handleSelectAllPress = () => {
+    selectAll(chapterList.map(chapter => chapter.id).concat(sharedChapterList.map(chapter => chapter.id)));
+  };
+
+  const handleDeselectAllPress = () => {
+    deSelectAll();
+  };
+
+  useEffect(() => {
+    if (!noteViewStore.isLongPressed) {
+      uiStore.setHeaderInfo({
+        title: '내 노트',
+        rightSide: [{ action: 'search', onClick: () => uiStore.toggleSearchBar() }],
+      });
+      return;
+    }
+
+    const isAllSelected = getSelectedCount() === chapterList.length + sharedChapterList.length;
+    uiStore.setHeaderInfo({
+      title: `${getSelectedCount()}개 선택됨`,
+      leftSide: [{ action: 'close', onClick: handleClosePress }],
+      rightSide: [
+        { action: 'delete', onClick: () => console.log('delete') },
+        { action: 'share', onClick: () => console.log('share') },
+        isAllSelected
+          ? { action: 'deselect', onClick: handleDeselectAllPress }
+          : { action: 'select', onClick: handleSelectAllPress },
+      ],
+    });
+  }, [noteViewStore.isLongPressed, getSelectedCount]);
+
   const fetchChapterList = async () => {
     const { normal, shared, recycle } = await chapterStore.getChapterList('79b3f1b3-85dc-4965-a8a2-0c4c56244b82'); // chId 관리 필요
     setChapterList(normal);
@@ -85,52 +108,9 @@ const NoteView: React.FC = () => {
 
   useLayoutEffect(() => {
     fetchChapterList();
-    uiStore.setHeaderInfo({
-      title: '내 노트',
-      rightSide: [{ action: 'search', onClick: () => uiStore.toggleSearchBar() }],
-    });
   }, []);
 
   return (
-    <>
-      {/* <Observer>
-        {() =>
-          noteViewStore.isLongPressed ? (
-            <NoteAppBar
-              title={`${getSelectedCount()}개 선택됨`}
-              isLongPress
-              closeFn={handleCloseButtonPress}
-              rightSide={[
-                { action: 'delete', onClick: () => console.log('delete') },
-                { action: 'share', onClick: () => console.log('share') },
-                {
-                  action: getSelectedCount() === chapterList.length + sharedChapterList.length ? 'deselect' : 'select',
-                  onClick:
-                    getSelectedCount() === chapterList.length + sharedChapterList.length
-                      ? handleDeselectAllPress
-                      : handleSelectAllPress,
-                },
-              ]}
-            />
-          ) : !searchBarVisible ? (
-            <NoteAppBar
-              title={title}
-              isLongPress={false}
-              rightSide={[
-                { action: 'search', onClick: () => handleSearchVisible() },
-                { action: 'character', onClick: () => console.log('character') },
-              ]}
-            />
-          ) : (
-            <SearchBar
-              value={getValue}
-              onChange={handleChange}
-              onEnter={() => handleSearch(getValue)}
-              onCancel={() => handleCancel(handleSearchVisible)}
-            />
-          )
-        }
-      </Observer> */}
       <NoteViewBodyWrapper>
         <Observer>
           {() =>
@@ -172,7 +152,7 @@ const NoteView: React.FC = () => {
                     {
                       variant: 'confirm',
                       text: '생성',
-                      onClick: (name: string) => handleCreateChapter(name),
+                    onClick: (name: string) => handleChapterCreate(name),
                     },
                   ]}
                 />
@@ -183,7 +163,6 @@ const NoteView: React.FC = () => {
           }
         </Observer>
       </NoteViewBodyWrapper>
-    </>
   );
 };
 
